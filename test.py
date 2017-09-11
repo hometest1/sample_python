e17324bbf904b1a965cb61f72c96d760fb10f2da
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-09-11 13:21:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
 import unittest
 from app import App
 
-class TestSuite(unittest.TestCase):
+class TestSuite1(unittest.TestCase1):
 
     def test(self):
         app = App()
         app.calculate()
         self.failIf(app.retrieve() != 62)
+        
+from app import App2
+
+class TestSuite2(unittest.TestCase2):
         app2 = App2()
         app2.calculate()
         self.failIf(app2.retrieve() != 66)
+        
+from app import App3
+
+class TestSuite3(unittest.TestCase3):
         app3 = App3()
         app3.calculate()
         self.failIf(app3.retrieve() != 70)
