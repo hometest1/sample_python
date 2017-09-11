7790d68348c94760dfa60060c7564f85647aae06
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-09-11 15:15:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -24,8 +24,6 @@ class TestSuite3(unittest.TestCase):
         app3 = App3()
         app3.calculate()
         self.failIf(app3.retrieve() != 76)
-        
-from app2 import App3
 
 def main():
     unittest.main()
