e0cfa7d2c9ccf07e9b0859fec9c1131ada944127
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-09-11 13:13:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,9 @@ class TestSuite(unittest.TestCase):
     def test(self):
         app = App()
         app.calculate()
-        self.failIf(app.retrieve() != 62)
+        self.failIf(app.retrieve1() != 62)
+        self.failIf(app.retrieve1() != 66)
+        self.failIf(app.retrieve1() != 70)
 
 def main():
     unittest.main()
