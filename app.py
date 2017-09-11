513a615932b746a5871cbfc66b89385e5dcc8691
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-09-11 13:11:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,32 @@ class App():
         self.var1 = 15
 
     def calculate(self):
-        self.result = self.var1 * 4 + 2
+        self.result1 = self.var1 * 4 + 2
 
-    def retrieve(self):
-        return self.result
+    def retrieve1(self):
+        return self.result1
     
     def __init__(self):
         self.var2 = 16
 
     def calculate(self):
-        self.result = self.var2 * 4 + 2
-        
+        self.result2 = self.var2 * 4 + 2
+    
+    def retrieve2(self):
+        return self.result2
+    
     def __init__(self):
         self.var3 = 17
 
     def calculate(self):
-        self.result = self.var3 * 4 + 2
-
+        self.result3 = self.var3 * 4 + 2
+    
+    def retrieve1(self):
+        return self.result3
+    
 if __name__ == "__main__":
     app = App()
     app.calculate()
-    print(app.retrieve)
+    print(app.retrieve1)
+    print(app.retrieve2)
+    print(app.retrieve3)
