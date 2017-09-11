e40a5d2673bd22e5f551059a9afb6b719c6ecdbe
shiphitchcock1 <shiphitchcock1@gmail.com>
2017-09-11 12:48:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,21 @@ class App():
     def retrieve(self):
         return self.result
 
+class App2():
+    def __init__(self):
+        self.var2 = 15
+
+    def calculate(self):
+        self.result = self.var2 * 4 + 1
+
+    def retrieve(self):
+        return self.result
+    
 if __name__ == "__main__":
     app = App()
+    app.calculate()
+    print(app.retrieve)
+    
+    app = App2()
     app.calculate()
     print(app.retrieve)
